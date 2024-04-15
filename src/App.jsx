9e4897d9f35277f55b2aedea7d6a0ead7cad29e7
derimer <jderimer@gmail.com>
2024-04-15 10:55:08 +0200

    utilisation props dans pokemonCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import "./components/PokemonCard";
 import PokemonCard from "./components/PokemonCard";
-
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 function App() {
   return (
     <div>
-      <PokemonCard></PokemonCard>
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 }
