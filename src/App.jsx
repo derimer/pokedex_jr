9a50d23160120cf8e44bb54141aae7255e0923cd
derimer <jderimer@gmail.com>
2024-04-16 16:33:45 +0200

    inscorporer useState au composant

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
+import React, { useState } from "react";
 
 const pokemonList = [
   {
@@ -8,15 +9,45 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <button
+        onClick={() => setPokemonIndex(pokemonIndex - 1)}
+        disabled={pokemonIndex === 0}
+      >
+        Précédent
+      </button>
+
+      <button
+        onClick={() => setPokemonIndex(pokemonIndex + 1)}
+        disabled={pokemonIndex === pokemonList.length - 1}
+      >
+        Suivant
+      </button>
+
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
     </div>
   );
 }
-
 export default App;
